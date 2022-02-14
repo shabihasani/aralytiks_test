d54a61f9dfe2b794713b8f83293e73334fd0dba9
FitonDurguti <Fiton.durguti11@gmail.com>
2022-02-14 08:42:45 +0100

    Create CSVtoPostgres.py

---
+++ CSVtoPostgres.py
@@ -0,0 +1,30 @@
+import psycopg2
+
+# connection establishment
+conn = psycopg2.connect(
+	database="postgres",
+	user='postgres',
+	password='Fiton',
+	host='localhost',
+	port='5432'
+)
+
+conn.autocommit = True
+
+# Creating a cursor object
+cursor = conn.cursor()
+
+# query to import data from given csv
+sql = '''copy demo FROM 
+        'C://Users//Aralytiks//Desktop//pythonProject1//Sample100.csv' 
+        DELIMITER ',' CSV HEADER'''
+
+# executing above query
+cursor.execute(sql)
+
+# Display the table
+cursor.execute('SELECT * FROM demo')
+print(cursor.fetchall())
+
+# Closing the connection
+conn.close()
